5ddc19d1c5ffaef22a636e43ecd05d8d1b33f52e
22Fatcorgi <wunn0916@gmail.com>
2026-03-21 21:35:11 +0800

    config Client LN Setting

--- config/stack_config.h
+++ config/stack_config.h
@@ -131,7 +131,7 @@
 #define CONFIG_IEC61850_BRCB_WITH_RESVTMS 1
 
 /* allow only configured clients (when pre-configured by ClientLN) - note behavior in PIXIT Rp13 */
-#define CONFIG_IEC61850_RCB_ALLOW_ONLY_PRECONFIGURED_CLIENT 0
+#define CONFIG_IEC61850_RCB_ALLOW_ONLY_PRECONFIGURED_CLIENT 1
 
 /* The default buffer size of buffered RCBs in bytes */
 #define CONFIG_REPORTING_DEFAULT_REPORT_BUFFER_SIZE 65536
